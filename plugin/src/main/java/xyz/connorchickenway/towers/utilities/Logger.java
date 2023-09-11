c63c571974855f681f0e744d8b4817c602c98949
ConnorChickenway <needforslowness@gmail.com>
2023-09-11 14:17:54 -0600

    refactor: add `Logger` class

---
+++ plugin/src/main/java/xyz/connorchickenway/towers/utilities/Logger.java
@@ -0,0 +1,22 @@
+package xyz.connorchickenway.towers.utilities;
+
+import java.util.logging.Level;
+
+import xyz.connorchickenway.towers.AmazingTowers;
+
+public class Logger 
+{
+
+    private static java.util.logging.Logger logger = AmazingTowers.getInstance().getLogger();
+    
+    public static void info( String info )
+    {
+        logger.info( info );
+    }
+
+    public static void error( Exception ex )
+    {
+        logger.log( Level.WARNING, "There is an exception!", ex );
+    }
+
+}
