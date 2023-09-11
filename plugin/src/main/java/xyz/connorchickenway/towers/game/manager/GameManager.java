a7dec41d5e44160e48bd0c83a707c1206199d5bc
ConnorChickenway <needforslowness@gmail.com>
2023-09-11 08:18:53 -0600

    refactor: related to a509167

--- plugin/src/main/java/xyz/connorchickenway/towers/game/manager/GameManager.java
+++ plugin/src/main/java/xyz/connorchickenway/towers/game/manager/GameManager.java
@@ -1,33 +1,28 @@
 package xyz.connorchickenway.towers.game.manager;
 
+import java.io.File;
 import java.util.Locale;
 import java.util.Map;
 
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
-import org.bukkit.event.Listener;
-import org.bukkit.event.entity.PlayerDeathEvent;
-import org.bukkit.event.player.AsyncPlayerChatEvent;
-
 import com.google.common.collect.Maps;
-
 import xyz.connorchickenway.towers.AmazingTowers;
-import xyz.connorchickenway.towers.config.StaticConfiguration;
 import xyz.connorchickenway.towers.game.Game;
-import xyz.connorchickenway.towers.game.entity.GamePlayer;
 import xyz.connorchickenway.towers.game.sign.manager.SignManager;
 import xyz.connorchickenway.towers.utilities.GameMode;
 import xyz.connorchickenway.towers.utilities.ManagerController;
 
-public class GameManager extends ManagerController implements Listener
+public class GameManager extends ManagerController
 {
 
     private Map<String, Game> games = Maps.newHashMap();
     private SignManager signManager;
+    private File gameFolder;
 
     public GameManager( AmazingTowers plugin )
     {
         super( plugin );
+        this.gameFolder = new File( plugin.getDataFolder(), "arenas" );
+        if ( !gameFolder.exists() ) gameFolder.mkdir();
     }
 
     @Override
@@ -35,7 +30,7 @@ public class GameManager extends ManagerController implements Listener
     {
         this.loadSignManager();
         this.loadGames();
-        plugin.getServer().getPluginManager().registerEvents( this, plugin );
+        plugin.getServer().getPluginManager().registerEvents( new GameListener(), plugin );
     }
 
     public void loadSignManager()
@@ -49,46 +44,50 @@ public class GameManager extends ManagerController implements Listener
 
     private void loadGames()
     {
-
+        
     }
 
     @Override
     public void disable()
     {
-        
+
     }
 
-    @EventHandler
-    public void onDeath( PlayerDeathEvent event )
+    public Game getFirstGame()
     {
-        GamePlayer gPlayer = plugin.getEntityManager().get( event.getEntity().getUniqueId() );
-        if ( !gPlayer.isInGame() ) return;
-        gPlayer.getGame().death( event );
-    }
-
-    @EventHandler( priority = EventPriority.MONITOR )
-    public void onChat( AsyncPlayerChatEvent event )
-    {
-        if ( event.isCancelled() || !StaticConfiguration.chat_enabled ) return;
-        GamePlayer gPlayer = plugin.getEntityManager().get( event.getPlayer().getUniqueId() );
-        if ( !gPlayer.isInGame() ) return;
-        gPlayer.getGame().chat( event );
+        for ( Game game : games.values() )
+            return game;
+        return null;
     }
 
     public Game getGame( String name )
     {
         return games.get( name.toLowerCase( Locale.ENGLISH ) );
     }
-    
+
+    public void addGame( Game game )
+    {
+        games.put( game.getGameName().toLowerCase(), game );
+    }
+
+    public boolean hasGame( String name )
+    {
+        return games.containsKey( name.toLowerCase() );
+    }
+
     public SignManager getSignManager()
     {
         return signManager;
+    }
+
+    public File getGameFolder()
+    {
+        return gameFolder;
     }
 
     public static GameManager get()
     {
         return AmazingTowers.getInstance().getGameManager();
     }
-    
 
 }
