5419483bcd2ee93df4b91337b0a124c0e80b3b93
ConnorChickenway <needforslowness@gmail.com>
2023-09-10 23:28:16 -0600

    refactor: add glow method

--- plugin/src/main/java/xyz/connorchickenway/towers/utilities/ItemUtils.java
+++ plugin/src/main/java/xyz/connorchickenway/towers/utilities/ItemUtils.java
@@ -1,23 +1,12 @@
 package xyz.connorchickenway.towers.utilities;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
-import org.bukkit.NamespacedKey;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.inventory.meta.ItemMeta;
-
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.reflect.TypeToken;
-
-import xyz.connorchickenway.towers.AmazingTowers;
-import xyz.connorchickenway.towers.nms.NMSVersion;
 
 public class ItemUtils 
 {
@@ -49,104 +38,21 @@ public class ItemUtils
         else 
             pInventory.setBoots( itemStack );    
     }
-    
-    @SuppressWarnings( "deprecation" )
-    public static JsonObject toJson( ItemStack itemStack )
+
+    public static void setGlow( ItemStack itemStack )
     {
-        JsonObject jObject = new JsonObject();
-        jObject.addProperty( "type", itemStack.getType().name() );
-        jObject.addProperty( "amount", itemStack.getAmount() );
-        if ( !NMSVersion.isNewerVersion )
-            if ( itemStack.getDurability() > 0 ) 
-                jObject.addProperty( "data", itemStack.getDurability() );
-        if ( itemStack.hasItemMeta() )
-        {
-            JsonObject metaObject = new JsonObject();
-            ItemMeta itemMeta = itemStack.getItemMeta();
-            metaObject.addProperty( "display_name", itemMeta.hasDisplayName() ? itemMeta.getDisplayName() : null );
-            if ( itemMeta.hasLore() )
-            {
-                JsonArray loreArray = new JsonArray();
-                itemMeta.getLore().forEach( loreArray::add );
-                metaObject.add( "lore", loreArray );
-            }
-            if ( itemMeta.hasEnchants() )
-            {
-                JsonArray enchantsArray = new JsonArray();
-                itemMeta.getEnchants().forEach( (enchantment, integer) -> enchantsArray.add( enchantment + ":" + integer ) );
-                metaObject.add( "enchants" , enchantsArray );
-            }
-            if ( !itemMeta.getItemFlags().isEmpty() ) 
-            {
-                JsonArray flags = new JsonArray();
-                itemMeta.getItemFlags().stream().map( ItemFlag::name ).forEach( flags::add );
-                metaObject.add( "flags", flags );
-            }
-            jObject.add( "item-meta", metaObject );
-        }
-        return jObject;
+        itemStack.addUnsafeEnchantment( Enchantment.LUCK, 1 );
+        ItemMeta meta = itemStack.getItemMeta();
+        meta.addItemFlags( ItemFlag.HIDE_ENCHANTS );
+        itemStack.setItemMeta( meta );
     }
 
-    @SuppressWarnings( "deprecation" )
-    public static ItemStack fromJson( JsonObject object ) throws Exception
+    public static void removeGlow( ItemStack itemStack )
     {
-        String type = object.get( "type" ).getAsString();
-        int amount = object.get( "amount" ).getAsInt();
-        byte durability = -1;
-        if ( !NMSVersion.isNewerVersion )
-            durability = object.get( "data" ).getAsByte();
-        JsonElement element = object.get( "item-meta" );
-        ItemStack itemStack = NMSVersion.isNewerVersion ? new ItemStack( Material.getMaterial( type ), amount ) : new ItemStack( Material.getMaterial( type ), amount, durability );
-        if ( element != null ) 
-        {
-            JsonObject metaObj = element.getAsJsonObject();
-            ItemMeta itemMeta = itemStack.getItemMeta();
-            String displayName = metaObj.get( "display_name" ).getAsString();
-            if ( displayName != null )
-                itemMeta.setDisplayName( displayName );
-            element = object.get( "lore" );
-            if ( element != null && element.isJsonArray() )
-            {
-                JsonArray jsonArray = element.getAsJsonArray();
-                List<String> list = AmazingTowers.GSON.fromJson( jsonArray, new TypeToken<ArrayList<String>>(){}.getType() );
-                itemMeta.setLore( list );
-            }
-            element = object.get( "enchants" );
-            if ( element != null && element.isJsonArray() )
-            {
-                JsonArray enchArray = element.getAsJsonArray();
-                enchArray.forEach( jElement -> 
-                {
-                    String[] split = jElement.getAsString().split( ":" );
-                    Enchantment enchantment = NMSVersion.isNewerVersion ? 
-                        Enchantment.getByKey( NamespacedKey.minecraft( split[ 0 ] ) ) :
-                        Enchantment.getByName( split[ 0 ] );
-                    int level = Integer.parseInt( split[ 1 ] );    
-                    if ( enchantment != null && level > 0)
-                        itemMeta.addEnchant( enchantment, level, true );
-                } );
-            }
-            element = object.get( "flags" );
-            if ( element != null && element.isJsonArray() )
-            {
-                JsonArray flagsArray = element.getAsJsonArray();
-                flagsArray.forEach( jElement -> 
-                {
-                    if ( jElement.isJsonPrimitive() )
-                    {
-                        for ( ItemFlag itemFlag : ItemFlag.values() )
-                        {
-                            if ( itemFlag.name().equalsIgnoreCase( jElement.getAsString() ) )
-                            {
-                                itemMeta.addItemFlags( itemFlag );
-                                break;
-                            }
-                        }
-                    }
-                } );
-            }
-        }
-        return itemStack;
+        itemStack.removeEnchantment( Enchantment.LUCK );
+        ItemMeta meta = itemStack.getItemMeta();
+        meta.removeItemFlags( ItemFlag.HIDE_ENCHANTS );
+        itemStack.setItemMeta( meta );
     }
     
 }
