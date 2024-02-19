626eb6ea8c4a3a57e779cd695890f586aa54d976
ConnorChickenway <needforslowness@gmail.com>
2024-02-19 16:42:18 -0600

    `refactor:` use inactive `slimeworldmanager-api`

--- plugin/src/main/java/xyz/connorchickenway/towers/game/world/SlimeWorldLoader.java
+++ plugin/src/main/java/xyz/connorchickenway/towers/game/world/SlimeWorldLoader.java
@@ -1,110 +1,88 @@
 package xyz.connorchickenway.towers.game.world;
 
-import java.io.File;
-import java.io.IOException;
-
-import org.bukkit.Bukkit;
-
 import com.grinderwolf.swm.api.exceptions.UnknownWorldException;
 import com.grinderwolf.swm.api.loaders.SlimeLoader;
 import com.grinderwolf.swm.api.world.SlimeWorld;
 import com.grinderwolf.swm.api.world.properties.SlimeProperties;
 import com.grinderwolf.swm.api.world.properties.SlimePropertyMap;
+import org.bukkit.Bukkit;
 import org.bukkit.GameRule;
 import org.bukkit.World;
 import xyz.connorchickenway.towers.nms.NMSVersion;
 
+import java.io.File;
+import java.io.IOException;
+
 import static xyz.connorchickenway.towers.AmazingTowers.SLIME_PLUGIN;
 
-public class SlimeWorldLoader implements GameWorld
-{
+public class SlimeWorldLoader implements GameWorld {
 
     private final String worldName;
-    
-    public SlimeWorldLoader( String worldName )
-    {
+
+    public SlimeWorldLoader(String worldName) {
         this.worldName = worldName;
     }
 
-    public SlimeWorldLoader( SlimeWorld slimeWorld )
-    {
-        this( slimeWorld.getName() );
+    public SlimeWorldLoader(SlimeWorld slimeWorld) {
+        this(slimeWorld.getName());
     }
 
     @Override
-    public boolean load()
-    {
+    public boolean load() {
         SlimeLoader loader = getLoader();
-        try
-        {
-            SlimeWorld slimeWorld = SLIME_PLUGIN.loadWorld( loader, worldName, true, getProperties() );
-            SLIME_PLUGIN.generateWorld( slimeWorld );
+        try {
+            SlimeWorld slimeWorld = SLIME_PLUGIN.loadWorld(loader, worldName, true, getProperties());
+            SLIME_PLUGIN.generateWorld(slimeWorld);
             World world = this.getWorld();
-            if ( NMSVersion.isNewerVersion )
-                world.setGameRule( GameRule.ANNOUNCE_ADVANCEMENTS, false );
+            if (NMSVersion.isNewerVersion)
+                world.setGameRule(GameRule.ANNOUNCE_ADVANCEMENTS, false);
             else
-                world.setGameRuleValue( "announceAdvancements", "false" );
+                world.setGameRuleValue("announceAdvancements", "false");
             return true;
-        } catch( Exception ignore ){}
+        } catch (Exception ignore) {
+        }
         return false;
     }
 
     @Override
-    public boolean unload( boolean save )
-    {
+    public boolean unload(boolean save) {
         SlimeLoader loader = getLoader();
-        try
-        {
-            Bukkit.unloadWorld( worldName, save );
-            loader.unlockWorld( worldName );
+        try {
+            Bukkit.unloadWorld(worldName, save);
+            loader.unlockWorld(worldName);
             return true;
-        } catch ( UnknownWorldException | IOException e )
-        {
+        } catch (UnknownWorldException | IOException e) {
             e.printStackTrace();
         }
         return false;
     }
 
     @Override
-    public void backup(){}
+    public void backup() {
+    }
 
     @Override
-    public String getWorldName()
-    {
+    public String getWorldName() {
         return worldName;
     }
 
     @Override
-    public File getBackupFolder()
-    {
+    public File getBackupFolder() {
         throw new UnsupportedOperationException("Unimplemented method 'getWorldName'");
     }
 
-    public static SlimePropertyMap getProperties()
-    {
+    public static SlimePropertyMap getProperties() {
         SlimePropertyMap properties = new SlimePropertyMap();
-        if ( NMSVersion.nmsVersion.isAboveOrEqual( NMSVersion.V1_16_R1 ) )
-        {
-            properties.setValue( SlimeProperties.DIFFICULTY, "peaceful" );
-            properties.setValue( SlimeProperties.ALLOW_ANIMALS, false );
-            properties.setValue( SlimeProperties.ALLOW_MONSTERS, false );
-            properties.setValue( SlimeProperties.PVP, true );
-            properties.setValue( SlimeProperties.ENVIRONMENT, "normal" );
-        }
-        else
-        {
-            properties.setString( SlimeProperties.DIFFICULTY, "peaceful" );
-            properties.setBoolean( SlimeProperties.ALLOW_ANIMALS, false );
-            properties.setBoolean( SlimeProperties.ALLOW_MONSTERS, false );
-            properties.setBoolean( SlimeProperties.PVP, true );
-            properties.setString( SlimeProperties.ENVIRONMENT, "normal" );
-        }
-        return properties; 
+        properties.setString(SlimeProperties.DIFFICULTY, "peaceful");
+        properties.setBoolean(SlimeProperties.ALLOW_ANIMALS, false);
+        properties.setBoolean(SlimeProperties.ALLOW_MONSTERS, false);
+        properties.setBoolean(SlimeProperties.PVP, true);
+        properties.setString(SlimeProperties.ENVIRONMENT, "normal");
+        return properties;
     }
 
-    public static SlimeLoader getLoader()
-    {
-        return SLIME_PLUGIN.getLoader( "file" );
+    public static SlimeLoader getLoader() {
+        return SLIME_PLUGIN.getLoader("file");
     }
 
 }
